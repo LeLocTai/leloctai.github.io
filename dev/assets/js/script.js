98fb98b86d4a2479a32be1fe631ea224906a0882
LeTai <vzttzv@gmail.com>
2016-12-28 21:41:31 +0700

    build only

--- dev/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var phone = $('.phone');
-    $('#canvas').attr({
-        width: phone.width() / 1.32 + 'px',
-        height: phone.height() / 1.45 + 'px'
-    });
-});
--- dev/js/script.js
+++
@@ -1,7 +0,0 @@
-// $(window).scroll(function() {
-//     if ($(document).scrollTop() > 25) {
-//         $('#topnav').addClass('scroll');
-//     } else {
-//         $('#topnav').removeClass('scroll');
-//     }
-// });
