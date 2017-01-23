254f42e4b2a55937bb08cf8b38ca4d2a73f42dba
LeTai <vzttzv@gmail.com>
2017-01-23 16:16:05 +0700

    A lot of change

--- asset/js/script.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("#loadingBox").hide()}),$("a.demo").click(function(){$("#loadingBox").show(),$("a.middle").hide();var e=document.createElement("script");e.src="Translucentwebgl/Release/UnityLoader.js",$("body").append(e)});
